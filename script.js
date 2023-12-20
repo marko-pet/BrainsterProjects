7a7bdca16bd56a6179c85158af449f5a52d26b81
Marko Petrovski <mpetrovski2005@gmail.com>
2023-12-20 16:46:57 +0100

    added filter options

--- script.js
+++ script.js
@@ -4,9 +4,63 @@ const navMenu = document.querySelector(".nav-menu");
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
-})
+});
 
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
-  hamburger.classList.remove("active");
-  navMenu.classList.remove("active");
-}))
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
+
+document
+  .querySelector("#filter-marketing")
+  .addEventListener("click", filterMarketing);
+document
+  .querySelector("#filter-programming")
+  .addEventListener("click", filterProgramming);
+document
+  .querySelector("#filter-design")
+  .addEventListener("click", filterDesign);
+
+function filterMarketing() {
+  var allCards = document.querySelectorAll(".card");
+
+  allCards.forEach((card) => {
+    card.style.display = "none";
+  });
+
+  var marketingCards = document.querySelectorAll(".marketing");
+
+  marketingCards.forEach((marketingCards) => {
+    marketingCards.style.display = "block";
+  });
+}
+
+function filterProgramming() {
+  var allCards = document.querySelectorAll(".card");
+
+  allCards.forEach((card) => {
+    card.style.display = "none";
+  });
+
+  var programmingCards = document.querySelectorAll(".programming");
+
+  programmingCards.forEach((programmingCards) => {
+    programmingCards.style.display = "block";
+  });
+}
+
+function filterDesign() {
+  var allCards = document.querySelectorAll(".card");
+
+  allCards.forEach((card) => {
+    card.style.display = "none";
+  });
+
+  var designCards = document.querySelectorAll(".design");
+
+  designCards.forEach((designCards) => {
+    designCards.style.display = "block";
+  });
+}
